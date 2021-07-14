1ded168adb1cb5cead11e98ce151810d99776ab9
Ammar Ahmed <ammarejaz1874@gmail.com>
2021-07-14 23:29:25 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -1,13 +1,13 @@
-provider "azurerm" {
-  features {}
-}
+#provider "azurerm" {
+  #features {}
+#}
 
 # Create a resource group if it doesn't exist
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
+#resource "azurerm_resource_group" "rg" {
+ # name     = var.resource_group_name
+  #location = var.resource_group_location
 
-  tags = {
-    environment = "salman"
-  }
-}
+  #tags = {
+   # environment = "salman"
+  #}
+#}
